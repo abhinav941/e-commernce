e3b326c26375435ed2da6467fb0791582ce07fc5
Abhinav Rao <abhinav.rao@gslab.com>
2020-07-21 21:12:06 +0530

    views added

--- server/controllers/shop.ts
+++ server/controllers/shop.ts
@@ -2,32 +2,30 @@ import Product from '../models/product';
 import Cart from '../models/cart';
 
 const getProducts = (req: any, res: any, next: any) => {
-  Product.fetchAll((products: any) => {
-    res.render('shop/product-list', { prods: products, docTitle: 'Shop', path: '/' });
-  });
+    const products = Product.findAll();
+    res.render('shop/product-list', { products: products, pageTitle: 'Shop', path: '/' });
 };
 
 const saveProductToCart = (req: any, res: any, next: any) => {};
 
 const getCheckout = (req: any, res: any, next: any) => {
-  res.render('shop/checkout', { path: '/checkout', docTitle: 'Checkout' });
+    res.render('shop/checkout', { path: '/checkout', pageTitle: 'Checkout' });
 };
 
 const getIndex = (req: any, res: any, next: any) => {
-  Product.fetchAll((products: any) => {
-    res.render('shop/index', { prods: products, docTitle: 'Shop', path: '/' });
-  });
+    const products = Product.findAll();
+    res.render('shop/index', { products: products, pageTitle: 'Shop', path: '/' });
 };
 
 const showCart = (req: any, res: any, next: any) => {
-  Cart.fetchCart((cart: any) => {
-    res.render('shop/cart', { cart, docTitle: 'Cart', path: '/cart' });
-  });
+    Cart.fetchCart((cart: any) => {
+        res.render('shop/cart', { cart, pageTitle: 'Cart', path: '/cart' });
+    });
 };
 
 export default {
-  getIndex,
-  showCart,
-  getProducts,
-  getCheckout,
+    getIndex,
+    showCart,
+    getProducts,
+    getCheckout,
 };
