22f9f6f88571fb9a99b5cee67905e9c14a0238cb
Abhinav Rao <abhinav.rao@gslab.com>
2020-07-21 18:33:59 +0530

    initial changes

--- src/components/ProductStack/ProductStack.js
+++ src/components/ProductStack/ProductStack.js
@@ -4,29 +4,25 @@ import ProductItem from './ProductItem/ProductItem';
 import { Component } from 'react';
 import axios from 'axios';
 
-
 class productStack extends Component {
-    state={
-        imageUrl:[],
+  state = {
+    imageUrl: [],
+  };
+  componentDidMount() {
+    axios.get('http://localhost:8080/images').then((response) => {
+      this.setState({ imageUrl: response.data.image_url });
+      console.log(response.data.image_url);
+    });
+  }
+  render() {
+    let products = null;
+    if (this.state.imageUrl) {
+      products = this.state.imageUrl.map((image) => {
+        return <ProductItem className="grid_item" key={image.url} url={image.url} price={image.price} />;
+      });
     }
-    componentDidMount(){
-        axios.get('http://localhost:3000/images').then((response)=>{
-            this.setState({imageUrl:response.data.image_url});
-                console.log(response.data.image_url);
-            }
-        );
-    }
-    render(){
-        let products=null;
-        if(this.state.imageUrl){
-            products=this.state.imageUrl.map((image)=>{
-                return <ProductItem className="grid_item" key={image.url} url={image.url} price={image.price}/>
-            });
-        }
-        return(<div className="stack">
-            {products}
-        </div>);
-    }
+    return <div className="stack">{products}</div>;
+  }
 }
 
 export default productStack;
