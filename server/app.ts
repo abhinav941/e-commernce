0b382eac2e91251580f94af2eb4e3eb166d47c53
Abhinav Rao <abhinav.rao@gslab.com>
2020-08-14 00:09:46 +0530

    html files added

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
-
+import path from 'path';
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('pages/index');
